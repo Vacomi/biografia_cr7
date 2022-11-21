ec67463e1b6b162b8f6515e853574555c89f4b02
Michael Vargas <vacomi.corp@gmail.com>
2022-11-21 15:58:20 -0500

    pagina contacto terminado

--- contacto.js
+++ contacto.js
@@ -6,9 +6,3 @@ function update(){
     progress.style.width=`${(window.scrollY)/(document.body.scrollHeight - window.innerHeight)*100}%`
     requestAnimationFrame(update)
 }
-
-const $form = document.getElementById('form');
-const $botoncito = document.getElementById('botoncito');
-$form.addEventListener('submit', () => {
-
-}) 
